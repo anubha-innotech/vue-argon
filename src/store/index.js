ab696654492ffe7183fea7838758d9852860096b
Anubha <anubha@innotechteam.in>
2022-11-14 15:55:15 +0530

    signup

---
+++ src/store/index.js
@@ -0,0 +1,66 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    hideConfigButton: false,
+    isPinned: true,
+    showConfig: false,
+    sidebarType: "bg-white",
+    isRTL: false,
+    mcolor: "",
+    darkMode: false,
+    isNavFixed: false,
+    isAbsolute: false,
+    showNavs: true,
+    showSidenav: true,
+    showNavbar: true,
+    showFooter: true,
+    showMain: true,
+    layout: "default",
+    user: null
+  },
+  mutations: {
+    toggleConfigurator(state) {
+      state.showConfig = !state.showConfig;
+    },
+    navbarMinimize(state) {
+      const sidenav_show = document.querySelector(".g-sidenav-show");
+
+      if (sidenav_show.classList.contains("g-sidenav-hidden")) {
+        sidenav_show.classList.remove("g-sidenav-hidden");
+        sidenav_show.classList.add("g-sidenav-pinned");
+        state.isPinned = true;
+      } else {
+        sidenav_show.classList.add("g-sidenav-hidden");
+        sidenav_show.classList.remove("g-sidenav-pinned");
+        state.isPinned = false;
+      }
+    },
+    sidebarType(state, payload) {
+      state.sidebarType = payload;
+    },
+    navbarFixed(state) {
+      if (state.isNavFixed === false) {
+        state.isNavFixed = true;
+      } else {
+        state.isNavFixed = false;
+      }
+    },
+    SET_USER(state,user) {
+      state.user = user
+    }
+  },
+  actions: {
+    toggleSidebarColor({ commit }, payload) {
+      commit("sidebarType", payload);
+    },
+    setUser({ commit }, user) {
+      commit('SET_USER', user)
+    },
+   },
+  getters: {
+    currentUser(state) {
+      return state.user
+    }
+  }
+});
