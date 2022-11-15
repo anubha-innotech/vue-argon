34c9d67c902862c3cfdf2d0e48d8fb9ebddabdd0
Anubha <anubha@innotechteam.in>
2022-11-15 21:57:03 +0530

    login with social handles

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,16 +1,16 @@
-// import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 // import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB7hVQKfXjKm1DrwSe7GCuhhxNR0kEVrWc",
-//   authDomain: "vue-argon-18d22.firebaseapp.com",
-//   projectId: "vue-argon-18d22",
-//   storageBucket: "vue-argon-18d22.appspot.com",
-//   messagingSenderId: "1030748009044",
-//   appId: "1:1030748009044:web:f3a0fec4f4741e0d5e33c1",
-//   measurementId: "G-JVX72Z953L"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyB7hVQKfXjKm1DrwSe7GCuhhxNR0kEVrWc",
+  authDomain: "vue-argon-18d22.firebaseapp.com",
+  projectId: "vue-argon-18d22",
+  storageBucket: "vue-argon-18d22.appspot.com",
+  messagingSenderId: "1030748009044",
+  appId: "1:1030748009044:web:f3a0fec4f4741e0d5e33c1",
+  measurementId: "G-JVX72Z953L"
+};
 
-// const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 // const auth = getAuth(app);
 // export { auth };
--- src/main.js
+++ src/main.js
@@ -5,26 +5,10 @@ import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import ArgonDashboard from "./argon-dashboard";
-
-import firebase from "firebase/compat/app";
-// require('firebase/auth');
-// import "firebase/auth";
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyB7hVQKfXjKm1DrwSe7GCuhhxNR0kEVrWc",
-    authDomain: "vue-argon-18d22.firebaseapp.com",
-    projectId: "vue-argon-18d22",
-    storageBucket: "vue-argon-18d22.appspot.com",
-    messagingSenderId: "1030748009044",
-    appId: "1:1030748009044:web:f3a0fec4f4741e0d5e33c1",
-    measurementId: "G-JVX72Z953L"
-  };
-
+import './firebase/index'
   
 const appInstance = createApp(App);
 appInstance.use(store);
 appInstance.use(router);
 appInstance.use(ArgonDashboard);
-firebase.initializeApp(firebaseConfig);
 appInstance.mount("#app");
